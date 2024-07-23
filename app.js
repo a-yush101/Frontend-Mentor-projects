511f3bb3ef5c2b203cf9ecd373d04317c0802c84
a-yush101 <164229465+a-yush101@users.noreply.github.com>
2024-07-23 22:11:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const emailInput = document.getElementById("emailInput");
+const submitBtn = document.getElementById("submitBtn");
+const dismissBtn = document.getElementById("dissMsg");
+const thankPage = document.querySelector(".thankYouBox");
+const inputPage = document.querySelector(".container");
+const errorMsg = document.querySelector(".errorMsg");
+
+
+submitBtn.addEventListener("click",(event) => {
+    event.preventDefault();
+    if(!validateEmail())
+    {
+        emailInput.classList.replace("emailInput","errorInput");
+        errorMsg.style.visibility = "visible";
+    }
+    else{
+        emailInput.classList.replace("errorInput","emailInput");
+        errorMsg.style.visibility = "hidden";
+        thankPage.style.display = "flex";
+        inputPage.style.display = "none";
+    }
+})
+    
+dismissBtn.onclick = () => {
+    thankPage.style.display = "none";
+    inputPage.style.display = "flex"
+}
+
+function validateEmail(){
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailRegex.test(emailInput.value)) {
+        return true;
+    } else {
+        return false;
+    }
+}
